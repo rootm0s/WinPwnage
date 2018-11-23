76580fdfa0b1e598683698b6f74d7ea63da5db83
rootm0s <7784475+rootm0s@users.noreply.github.com>
2018-11-24 00:47:45 +0100

    Update winpwnage.py

--- winpwnage.py
+++ winpwnage.py
@@ -20,13 +20,13 @@ print_info("Running elevated: {}\n".format(information().admin()))
 def main():
 	try:
 		if sys.argv[1].lower() == "-scan":
-				scanner().start()
+			scanner().start()
 		elif sys.argv[1].lower() == "-uac":
-				function(uac=True, persist=False).run(id=sys.argv[2], payload=sys.argv[3])
+			function(uac=True, persist=False).run(id=sys.argv[2], payload=sys.argv[3])
 		elif sys.argv[1].lower() == "-add_persist":
-				function(uac=False, persist=True).run(id=sys.argv[2], payload=sys.argv[3], name=sys.argv[4], add=True)
+			function(uac=False, persist=True).run(id=sys.argv[2], payload=sys.argv[3], name=sys.argv[4], add=True)
 		elif sys.argv[1].lower() == "-remove_persist":
-				function(uac=False, persist=True).run(id=sys.argv[2], payload=sys.argv[3], name=sys.argv[4], add=False)
+			function(uac=False, persist=True).run(id=sys.argv[2], payload=sys.argv[3], name=sys.argv[4], add=False)
 	except Exception as error:
 		pass
 
