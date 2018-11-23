1820fd91aba79f1707ab298280ce9f416bbf8df1
rootm0s <7784475+rootm0s@users.noreply.github.com>
2018-11-23 23:15:23 +0100

    Update winpwnage.py

--- winpwnage.py
+++ winpwnage.py
@@ -17,26 +17,6 @@ print_info("UAC level: {}".format(information().uac_level()))
 print_info("Build number: {}".format(information().build_number()))
 print_info("Running elevated: {}\n".format(information().admin()))
 
-
-import sys
-from winpwnage.core.prints import print_info
-from winpwnage.core.scanner import scanner, function
-from winpwnage.core.utils import information
-
-banner = """
-        _                               
-  _ _ _|_|___ ___ _ _ _ ___ ___ ___ ___ 
- | | | | |   | . | | | |   | .'| . | -_|
- |_____|_|_|_|  _|_____|_|_|__,|_  |___|
-             |_|               |___|
-"""
-
-print(banner)
-
-print_info("UAC level: {}".format(information().uac_level()))
-print_info("Build number: {}".format(information().build_number()))
-print_info("Running elevated: {}\n".format(information().admin()))
-
 def main():
 	try:
 		if sys.argv[1].lower() == "-scan":
