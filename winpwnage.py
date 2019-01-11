666ed95a5d6f5724aac43ff5688503ae309332c4
rootm0s <7784475+rootm0s@users.noreply.github.com>
2019-01-11 09:13:02 +0100

    elevate from administrator to system

--- winpwnage.py
+++ winpwnage.py
@@ -22,24 +22,32 @@ def main():
 	# Scanner
 	#
 	if sys.argv[1].lower() == "-scan" and sys.argv[2].lower() == "-uac":
-		scanner(uac=True, persist=False).start()
+		scanner(uac=True, persist=False, elevate=False).start()
 	elif sys.argv[1].lower() == "-scan" and sys.argv[2].lower() == "-persist":
-		scanner(uac=False, persist=True).start()		
+		scanner(uac=False, persist=True, elevate=False).start()
+	elif sys.argv[1].lower() == "-scan" and sys.argv[2].lower() == "-elevate":
+		scanner(uac=False, persist=False, elevate=True).start()
 
 	#
 	# UAC bypass
 	#
 	elif sys.argv[1].lower() == "-use" and sys.argv[2].lower() == "-uac":
-		function(uac=True, persist=False).run(id=sys.argv[3], payload=sys.argv[4])
-
+		function(uac=True, persist=False, elevate=False).run(id=sys.argv[3], payload=sys.argv[4])		
+		
 	#
 	# Persistence
 	#
 	elif sys.argv[1].lower() == "-use" and sys.argv[2].lower() == "-persist" and sys.argv[3].lower() == "-add":
-		function(uac=False, persist=True).run(id=sys.argv[4], payload=sys.argv[5], add=True)
+		function(uac=False, persist=True, elevate=False).run(id=sys.argv[4], payload=sys.argv[5], add=True)
 
 	elif sys.argv[1].lower() == "-use" and sys.argv[2].lower() == "-persist" and sys.argv[3].lower() == "-remove":
-		function(uac=False, persist=True).run(id=sys.argv[4], payload=sys.argv[5], add=False)
+		function(uac=False, persist=True, elevate=False).run(id=sys.argv[4], payload=sys.argv[5], add=False)
 
+	#
+	# Elevate
+	#
+	elif sys.argv[1].lower() == "-use" and sys.argv[2].lower() == "-elevate":
+		function(uac=False, persist=False, elevate=True).run(id=sys.argv[3], payload=sys.argv[4])		
+		
 if __name__ == "__main__":
 	main()
