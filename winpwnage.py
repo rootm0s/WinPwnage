dee2b98abbe6aaf0cdaf9d03e1394762b4c4091c
rootm0s <7784475+rootm0s@users.noreply.github.com>
2019-02-08 16:14:41 +0100

    execute methods added to bypass applocker etc

--- winpwnage.py
+++ winpwnage.py
@@ -3,15 +3,13 @@ from winpwnage.core.prints import print_info
 from winpwnage.core.scanner import scanner, function
 from winpwnage.core.utils import information
 
-banner = """
+print """
         _                               
   _ _ _|_|___ ___ _ _ _ ___ ___ ___ ___ 
  | | | | |   | . | | | |   | .'| . | -_|
  |_____|_|_|_|  _|_____|_|_|__,|_  |___|
              |_|               |___|
 """
-
-print(banner)
 
 print_info("UAC level: {}".format(information().uac_level()))
 print_info("Build number: {}".format(information().build_number()))
@@ -22,32 +20,45 @@ def main():
 	# Scanner
 	#
 	if sys.argv[1].lower() == "-scan" and sys.argv[2].lower() == "-uac":
-		scanner(uac=True, persist=False, elevate=False).start()
+		scanner(uac=True, persist=False, elevate=False, execute=False).start()
 	elif sys.argv[1].lower() == "-scan" and sys.argv[2].lower() == "-persist":
-		scanner(uac=False, persist=True, elevate=False).start()
+		scanner(uac=False, persist=True, elevate=False, execute=False).start()
 	elif sys.argv[1].lower() == "-scan" and sys.argv[2].lower() == "-elevate":
-		scanner(uac=False, persist=False, elevate=True).start()
+		scanner(uac=False, persist=False, elevate=True, execute=False).start()
+	elif sys.argv[1].lower() == "-scan" and sys.argv[2].lower() == "-execute":
+		scanner(uac=False, persist=False, elevate=False, execute=True).start()
 
 	#
 	# UAC bypass
 	#
 	elif sys.argv[1].lower() == "-use" and sys.argv[2].lower() == "-uac":
-		function(uac=True, persist=False, elevate=False).run(id=sys.argv[3], payload=sys.argv[4])		
-		
+		function(uac=True, persist=False, elevate=False,
+				execute=False).run(id=sys.argv[3], payload=sys.argv[4])		
+
 	#
 	# Persistence
 	#
 	elif sys.argv[1].lower() == "-use" and sys.argv[2].lower() == "-persist" and sys.argv[3].lower() == "-add":
-		function(uac=False, persist=True, elevate=False).run(id=sys.argv[4], payload=sys.argv[5], add=True)
+		function(uac=False, persist=True, elevate=False,
+				execute=False).run(id=sys.argv[4], payload=sys.argv[5], add=True)
 
 	elif sys.argv[1].lower() == "-use" and sys.argv[2].lower() == "-persist" and sys.argv[3].lower() == "-remove":
-		function(uac=False, persist=True, elevate=False).run(id=sys.argv[4], payload=sys.argv[5], add=False)
+		function(uac=False, persist=True, elevate=False,
+				execute=False).run(id=sys.argv[4], payload=sys.argv[5], add=False)
 
 	#
 	# Elevate
 	#
 	elif sys.argv[1].lower() == "-use" and sys.argv[2].lower() == "-elevate":
-		function(uac=False, persist=False, elevate=True).run(id=sys.argv[3], payload=sys.argv[4])		
-		
+		function(uac=False, persist=False, elevate=True,
+				execute=False).run(id=sys.argv[3], payload=sys.argv[4])		
+
+	#
+	# Execute
+	#
+	elif sys.argv[1].lower() == "-use" and sys.argv[2].lower() == "-execute":
+		function(uac=False, persist=False, elevate=False,
+				execute=True).run(id=sys.argv[3], payload=sys.argv[4])
+
 if __name__ == "__main__":
 	main()
