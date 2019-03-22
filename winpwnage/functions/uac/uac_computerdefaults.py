9efadc9ea193f67b461fa40d4f3c35f9af2da488
cclauss <cclauss@me.com>
2019-03-22 10:20:32 +0100

    Remove unused import '_winreg'

--- winpwnage/functions/uac/uac_computerdefaults.py
+++ winpwnage/functions/uac/uac_computerdefaults.py
@@ -1,9 +1,5 @@
 import os
 import time
-try:
-	import _winreg   # Python 2
-except ImportError:      # Python 3
-	import winreg as _winreg
 from winpwnage.core.prints import *
 from winpwnage.core.utils import *
 
