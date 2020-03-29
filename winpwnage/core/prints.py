e39088439e12d8aab37e4955d09b203e52bffe5e
rootm0s <rootm0s@protonmail.com>
2020-03-29 03:42:11 +0200

    colorama is now optional

--- winpwnage/core/prints.py
+++ winpwnage/core/prints.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
 try:
+	status = None
 	from colorama import init, Fore
 except:
-	pass
+	status = False
 else:
 	init(convert=True)
 
@@ -25,30 +26,44 @@ def print_table():
 
 
 def table_success(id, message):
-	print("\033[92m" + " " + id + "\033[0m" + message)
+	if status == False:
+		print("" + " " + id + message)
+	else:
+		print(Fore.GREEN + " " + id + Fore.RESET + message)
 	Constant.output.append(("ok", id + message))
 
 
 def table_error(id, message):
-	print("\033[31m" + " " + id + "\033[0m" + message)
+	if status == False:
+		print("" + " " + id + message)
+	else:
+		print(Fore.RED + " " + id + Fore.RESET + message)
 	Constant.output.append(("error", id + message))
 
-
 def print_success(message):
-	print("\033[92m" + " [+] " + "\033[0m" + message)
+	if status == False:
+		print(" [+] " + message)
+	else:
+		print(Fore.GREEN + " [+] " + Fore.RESET + message)
 	Constant.output.append(("ok", message))
 
-
 def print_error(message):
-	print("\033[31m" + " [-] " + "\033[0m" + message)
+	if status == False:
+		print(" [-] " + message)
+	else:
+		print(Fore.RED + " [-] " + Fore.RESET + message)
 	Constant.output.append(("error", message))
 
-
 def print_info(message):
-	print("\033[36m" + " [!] " + "\033[0m" + message)
+	if status == False:
+		print(" [!] " + message)
+	else:
+		print(Fore.CYAN + " [!] " + Fore.RESET + message)
 	Constant.output.append(("info", message))
 
-
 def print_warning(message):
-	print("\033[93m" + " [!] " + "\033[0m" + message)
+	if status == False:
+		print(" [!] " + message)
+	else:
+		print(Fore.YELLOW + " [+] " + Fore.RESET + message)
 	Constant.output.append(("warning", message))
